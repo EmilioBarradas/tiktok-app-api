5acf55c131b9af2bf130c9a2cfa25c75cae19ed3
Decalers <decalerss@gmail.com>
2020-05-26 17:44:46 -0400

    Updated web-api to use new tiktok-api

--- web-api/server.ts
+++ web-api/server.ts
@@ -1,25 +1,77 @@
-const express = require('express');
-const tiktok = require('../tiktok-api/api');
+import express = require('express');
+import tiktok = require('../tiktok-api/api');
 
 const app = express();
 
-app.get('/api/user/:username', getUser);
-app.get('/api/user/:user/videos', getUserVideos);
+app.get('/api/user/:identifier', getUserInfo);
+app.get('/api/user/:identifier/videos', getRecentVideos);
+app.get('/api/user/:identifier/liked', getLikedVideos);
 
-app.get('/api/video/:video', getVideoInfo);
+app.get('/api/video/:id', getVideoInfo);
+
+app.get('/api/audio/:id', getAudioInfo);
+app.get('/api/audio/:id/videos', getAudioTopVideos);
+
+app.get('/api/tag/:id', getTagInfo);
+app.get('/api/tag/:id', getTagTopVideos);
 
 app.listen(8000);
 
-async function getUser(req, res) {
-    res.status(200).send(await tiktok.getUser(req.params.username));
+async function getUserInfo(req, res) {
+    const userInfo = await tiktok.getUserInfo(req.params.identifier);
+
+    res.status(200).send(userInfo).end();
 }
 
-async function getUserVideos(req, res) {
-    const user = isNaN(req.params.user) ? await tiktok.getUser(req.params.user) : { id: req.params.user };
-    res.status(200).send(await tiktok.getUserVideos(user));
+async function getRecentVideos(req, res) {
+    const user = await getUser(req.params.identifier);
+    const recentVideos = await tiktok.getRecentVideos(user);
+
+    res.status(200).send(recentVideos).end();
+}
+
+async function getLikedVideos(req, res) {
+    const user = await getUser(req.params.identifier);
+    const likedVideos = await tiktok.getLikedVideos(user);
+
+    res.status(200).send(likedVideos).end();
 }
 
 async function getVideoInfo(req, res) {
-    const video = await tiktok.getVideo(req.params.video);
-    res.status(200).send(await tiktok.getVideoInfo(video));
+    const video = await tiktok.getVideo(req.params.id);
+    const videoInfo = await tiktok.getVideoInfo(video);
+
+    res.status(200).send(videoInfo).end();
+}
+
+async function getAudioInfo(req, res) {
+    const audio = await tiktok.getAudio(req.params.id);
+    const audioInfo = await tiktok.getAudioInfo(audio);
+
+    res.status(200).send(audioInfo).end();
+}
+
+async function getAudioTopVideos(req, res) {
+    const audio = await tiktok.getAudio(req.params.id);
+    const topVideos = await tiktok.getAudioTopVideos(audio);
+
+    res.status(200).send(topVideos).end();
+}
+
+async function getTagInfo(req, res) {
+    const tag = await tiktok.getTag(req.params.id);
+    const tagInfo = await tiktok.getTagInfo(tag);
+
+    res.status(200).send(tagInfo).end();
+}
+
+async function getTagTopVideos(req, res) {
+    const tag = await tiktok.getTag(req.params.id);
+    const topVideos = await tiktok.getTagTopVideos(tag);
+
+    res.status(200).send(topVideos).end();
+}
+
+async function getUser(id: string) {
+    return isNaN(Number(id)) ? await tiktok.getUserByName(id) : Promise.resolve(tiktok.getUserByID(id));
 }
